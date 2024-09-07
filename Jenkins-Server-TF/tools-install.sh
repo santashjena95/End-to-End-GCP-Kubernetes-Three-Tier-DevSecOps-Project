55e7ef487491b30dc89cbe32c000927352f77419
santashjena95 <jenasantash95@gmail.com>
2024-09-07 14:40:01 +0530

    Updated startupscript

--- Jenkins-Server-TF/tools-install.sh
+++ Jenkins-Server-TF/tools-install.sh
@@ -31,14 +31,6 @@ sudo chmod 777 /var/run/docker.sock
 #!/bin/bash
 docker run -d  --name sonar -p 9000:9000 sonarqube:lts-community
 
-
-# Installing AWS CLI
-#!/bin/bash
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo apt install unzip -y
-unzip awscliv2.zip
-sudo ./aws/install
-
 # Installing Kubectl
 #!/bin/bash
 sudo apt update
@@ -47,13 +39,6 @@ sudo curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
 sudo chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 kubectl version --client
-
-
-# Installing eksctl
-#! /bin/bash
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-sudo mv /tmp/eksctl /usr/local/bin
-eksctl version
 
 # Installing Terraform
 #!/bin/bash
